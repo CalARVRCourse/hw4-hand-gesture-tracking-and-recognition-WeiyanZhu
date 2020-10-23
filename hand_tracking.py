7b1f444d617510059fd13b3648680ff89468180d
Weiyan Zhu <weiyanz@berkeley.edu>
2020-10-23 15:17:18 -0700

    finish part one

--- hand_tracking.py
+++ hand_tracking.py
@@ -1,41 +1,104 @@
+from __future__ import print_function
 import cv2
+import argparse
 import numpy as np
-import glob
 
-# This function records images from the connected camera to specified directory 
-# when the "Space" key is pressed.
-# directory: should be a string corresponding to the name of an existing 
-# directory
-def CaptureImages(directory):
-    # Open the camera for capture
-    # the 0 value should default to the webcam, but you may need to change this
-    # for your camera, especially if you are using a camera besides the default
-    cam = cv2.VideoCapture(0)
-    img_counter = 0
-    # Read until user quits
-    while True:
-        ret, frame = cam.read()
-        if not ret:
-            break
-        # display the current image
-        cv2.imshow("Display", frame)
-        '''
-        def nothing(x):  
-            pass  
-        cv2.namedWindow("tracks")    
-        # Create a Trackbar to choose a value for a parameter    
-        cv2.createTrackbar(parameter_value_name, "tracks" , parameter_min_value, parameter_max_value, nothing)  
-        '''
-        # wait for 1ms or key press
-        k = cv2.waitKey(1) #k is the key pressed
-        if k == 27 or k==113:  #27, 113 are ascii for escape and q respectively
-            #exit
-            break
-        elif k == 32: #32 is ascii for space
-            #record image
-            img_name = "captured_image_{}.png".format(img_counter)
-            cv2.imwrite(directory+'/'+img_name, frame)
-            print("Writing: {}".format(directory+'/'+img_name))
-            img_counter += 1
-    cam.release()
+max_value = 255
+max_type = 4
+max_binary_value = 255
+trackbar_type = 'Type: \n 0: Binary \n 1: Binary Inverted \n 2: Truncate \n 3: To Zero \n 4: To Zero Inverted'
+trackbar_value = 'Value'
+trackbar_blur = 'Blur kernel size'
+window_name = 'Threshold Demo'
+isColor = False
 
+
+def nothing(x):
+    pass
+    
+
+cam = cv2.VideoCapture(0)
+cv2.namedWindow(window_name)
+cv2.createTrackbar(trackbar_type, window_name , 3, max_type, nothing)
+# Create Trackbar to choose Threshold value
+cv2.createTrackbar(trackbar_value, window_name , 0, max_value, nothing)
+# Call the function to initialize
+cv2.createTrackbar(trackbar_blur, window_name , 1, 20, nothing)
+# create switch for ON/OFF functionality
+color_switch = 'Color'
+cv2.createTrackbar(color_switch, window_name,0,1,nothing)
+cv2.createTrackbar('Contours', window_name,0,1,nothing)
+
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        cv2.destroyAllWindows()
+        cam.release()
+        break
+    
+    lower_HSV = np.array([0, 40, 0], dtype = "uint8")  
+    upper_HSV = np.array([25, 255, 255], dtype = "uint8")  
+      
+    convertedHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  
+    skinMaskHSV = cv2.inRange(convertedHSV, lower_HSV, upper_HSV)  
+      
+      
+    lower_YCrCb = np.array((0, 138, 67), dtype = "uint8")  
+    upper_YCrCb = np.array((255, 173, 133), dtype = "uint8")  
+          
+    convertedYCrCb = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)  
+    skinMaskYCrCb = cv2.inRange(convertedYCrCb, lower_YCrCb, upper_YCrCb)  
+      
+    skinMask = cv2.add(skinMaskHSV,skinMaskYCrCb)  
+    
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))  
+    skinMask = cv2.erode(skinMask, kernel, iterations = 2)  
+    skinMask = cv2.dilate(skinMask, kernel, iterations = 2)  
+      
+    # blur the mask to help remove noise, then apply the  
+    # mask to the frame  
+    skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0) 
+    skin = cv2.bitwise_and(frame, frame, mask = skinMask) 
+    
+    #0: Binary
+    #1: Binary Inverted
+    #2: Threshold Truncated
+    #3: Threshold to Zero
+    #4: Threshold to Zero Inverted
+    threshold_type = cv2.getTrackbarPos(trackbar_type, window_name)
+    threshold_value = cv2.getTrackbarPos(trackbar_value, window_name)
+    blur_value = cv2.getTrackbarPos(trackbar_blur, window_name)
+    blur_value = blur_value+ (  blur_value%2==0)
+    isColor = (cv2.getTrackbarPos(color_switch, window_name) == 1)
+    findContours = (cv2.getTrackbarPos('Contours', window_name) == 1)
+    
+    #convert to grayscale
+    if isColor == False:
+        src_gray = cv2.cvtColor(skin, cv2.COLOR_BGR2GRAY)
+        _, dst = cv2.threshold(src_gray, threshold_value, max_binary_value, threshold_type )
+        blur = cv2.GaussianBlur(dst,(blur_value,blur_value),0)
+        if findContours:
+            _, contours, hierarchy = cv2.findContours( blur, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE )
+            blur = cv2.cvtColor(blur, cv2.COLOR_GRAY2BGR)  #add this line
+            output = cv2.drawContours(blur, contours, -1, (0, 255, 0), 1)
+            print(str(len(contours))+"\n")
+        else:
+            output = blur
+        
+        
+    else:
+        _, dst = cv2.threshold(skin, threshold_value, max_binary_value, threshold_type )
+        blur = cv2.GaussianBlur(dst,(blur_value,blur_value),0)
+        output = blur
+    
+    
+    cv2.imshow(window_name, output)
+
+    
+    k = cv2.waitKey(1) #k is the key pressed
+    if k == 27 or k==113:  #27, 113 are ascii for escape and q respectively
+        #exit
+        cv2.destroyAllWindows()
+        cam.release()
+        break
